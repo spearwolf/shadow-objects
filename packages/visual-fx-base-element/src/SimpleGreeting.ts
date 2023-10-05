603c4af3feab87cd282190b947aaf7cd8d2d4df6
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-05 15:01:39 +0200

    WIP remove lit?

--- packages/visual-fx-base-element/src/SimpleGreeting.ts
+++ packages/visual-fx-base-element/src/SimpleGreeting.ts
@@ -1,14 +1,30 @@
+import {createEffect, signal} from '@spearwolf/signalize';
 import {LitElement, css, html} from 'lit';
-import {property} from 'lit/decorators';
 
 export class SimpleGreeting extends LitElement {
+  static override properties: {
+    name: {type: String};
+  };
+
+  declare name: string;
+
   static override styles = css`
     p {
       color: blue;
     }
   `;
 
-  @property() accessor name = 'Somebody';
+  @signal() accessor foo = 'bar';
+
+  constructor() {
+    super();
+
+    this.name = 'Somebody';
+
+    createEffect(() => {
+      console.log('foo changed to', this.foo);
+    });
+  }
 
   override render() {
     return html`<p>Hello, ${this.name}!</p>`;
