8b91a1a671d00d8e8f9e41c2bc25902cdd9529de
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-15 16:52:05 +0200

    refactor comments

--- packages/shadow-ents/src/view-components/ViewComponent.ts
+++ packages/shadow-ents/src/view-components/ViewComponent.ts
@@ -1,21 +1,13 @@
 import {ComponentContext} from './ComponentContext.js';
 import {generateUUID} from './generateUUID.js';
 
-/**
- * The EntityView is a proxy for the actual entity object.
- * With the help of this digital twin the _real_ entity is created, properties are set, events are triggered, etc.
- *
- * While the user is responsible for creating a twin, the actual entities are later managed async by the EntityKernel.
- * The kernel can, but does not have to, run in the same javascript environment. It is also conceivable, for example,
- * that the kernel runs in a web-worker, while the entity twins are created within the main document.
- */
 export class ViewComponent {
   #uuid: string;
   #token: string;
   #namespace?: string | symbol;
   #order = 0;
 
-  #viewspace: ComponentContext;
+  #context: ComponentContext;
   #parent?: ViewComponent;
 
   get uuid() {
@@ -39,7 +31,8 @@ export class ViewComponent {
   }
 
   /**
-   * The order property sets the order to lay out an entity in a children array of the parent entity container.
+   * The order property sets the order to lay out a component in a children array of the parent component.
+   *
    * Items in a children array are sorted by ascending order value and then by their insertion order.
    */
   get order(): number {
@@ -50,7 +43,7 @@ export class ViewComponent {
     const prevOrder = this.#order;
     this.#order = order ?? 0;
     if (prevOrder !== this.#order) {
-      this.#viewspace.changeOrder(this);
+      this.#context.changeOrder(this);
     }
   }
 
@@ -62,8 +55,8 @@ export class ViewComponent {
     this.#order = order;
     this.#namespace = namespace;
 
-    this.#viewspace = ComponentContext.get(this.#namespace);
-    this.#viewspace.addComponent(this);
+    this.#context = ComponentContext.get(this.#namespace);
+    this.#context.addComponent(this);
   }
 
   isChildOf(entity: ViewComponent) {
@@ -72,7 +65,7 @@ export class ViewComponent {
 
   removeFromParent() {
     if (this.#parent) {
-      this.#viewspace.removeFromParent(this.uuid, this.#parent);
+      this.#context.removeFromParent(this.uuid, this.#parent);
       this.#parent = undefined;
     }
   }
@@ -81,20 +74,20 @@ export class ViewComponent {
     if (!child.isChildOf(this)) {
       child.removeFromParent();
       child.#parent = this;
-      this.#viewspace.addToChildren(this, child);
+      this.#context.addToChildren(this, child);
     }
   }
 
   setProperty<T = unknown>(name: string, value: T, isEqual?: (a: T, b: T) => boolean) {
-    this.#viewspace.setProperty(this, name, value, isEqual);
+    this.#context.setProperty(this, name, value, isEqual);
   }
 
   removeProperty(name: string) {
-    this.#viewspace.removeProperty(this, name);
+    this.#context.removeProperty(this, name);
   }
 
   destroy() {
     this.removeFromParent();
-    this.#viewspace.removeComponent(this);
+    this.#context.removeComponent(this);
   }
 }
