1f434e15cc4e9d93d82133f220a50851bd3fd8d4
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-16 13:41:58 +0200

    request context element event

--- packages/shadow-ents/src/elements/ShadowEntsElement.ts
+++ packages/shadow-ents/src/elements/ShadowEntsElement.ts
@@ -12,41 +12,46 @@ export class ShadowEntsElement extends HTMLElement {
     super();
 
     this[$uuid] = generateUUID();
-
-    this.addEventListener(RequestContextEventName, this.#onRequestContext.bind(this), {passive: false});
-    this.addEventListener(ElementContextEventName, this.#onElementContext.bind(this), {passive: false});
   }
 
   connectedCallback() {
-    console.log('Custom element added to page.');
+    console.log('Custom element added to page', this[$uuid]);
 
-    this.dispatchEvent(new CustomEvent(RequestContextEventName, {detail: {requester: this}}));
+    this.dispatchEvent(new CustomEvent(RequestContextEventName, {bubbles: true, detail: {requester: this}}));
+
+    this.addEventListener(RequestContextEventName, this.#onRequestContext, {capture: false, passive: false});
+    this.addEventListener(ElementContextEventName, this.#onElementContext, {capture: false, passive: false});
   }
 
   disconnectedCallback() {
-    console.log('Custom element removed from page.');
+    console.log('Custom element removed from page', this[$uuid]);
+
+    this.removeEventListener(ElementContextEventName, this.#onElementContext, {capture: false});
+    this.removeEventListener(RequestContextEventName, this.#onRequestContext, {capture: false});
   }
 
   adoptedCallback() {
-    console.log('Custom element moved to new page.');
+    console.log('Custom element moved to new page', this[$uuid]);
   }
 
-  #onRequestContext(event: RequestContextEvent) {
-    console.log('Custom element request context', event);
-
+  #onRequestContext = (event: RequestContextEvent) => {
     const requester = event.detail?.requester;
+
+    console.log('Custom element request context', {requester: requester?.[$uuid], self: this[$uuid]});
+
     if (requester != null && requester !== this) {
+      console.log('Custom element stop propagation', requester);
       event.stopPropagation();
       requester.#setContextElement(this);
     }
-  }
+  };
 
   #setContextElement(contextElement: ShadowEntsElement) {
     console.log('Custom element set context element', contextElement);
   }
 
   // TODO remove?
-  #onElementContext(event: ElementContextEvent) {
+  #onElementContext = (event: ElementContextEvent) => {
     console.log('Custom element element context', event);
-  }
+  };
 }
