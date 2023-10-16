62146f30c2d6c898dc0e73dea47cef3e758a8970
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-16 14:41:45 +0200

    rename uuid

--- packages/shadow-ents/src/elements/ShadowEntsElement.ts
+++ packages/shadow-ents/src/elements/ShadowEntsElement.ts
@@ -1,43 +1,55 @@
+import {createEffect, createSignal, type SignalReader, type SignalWriter} from '@spearwolf/signalize';
 import {generateUUID} from '../generateUUID';
-import {$isElement, $type, $uuid, ElementContextEventName, ElementType, RequestContextEventName} from './constants';
-import type {ElementContextEvent, RequestContextEvent} from './events';
+import {$isElement, $type, ElementType, RequestContextEventName} from './constants';
+import type {RequestContextEvent} from './events';
+
+// TODO namespace
+// TODO children + order
 
 export class ShadowEntsElement extends HTMLElement {
   readonly [$isElement] = true;
 
-  readonly [$uuid]: string;
   readonly [$type]: ElementType = ElementType.Base;
+
+  readonly uuid = generateUUID();
+
+  #contextElementSignal: [SignalReader<ShadowEntsElement | undefined>, SignalWriter<ShadowEntsElement | undefined>] =
+    createSignal();
+
+  get contextElement(): ShadowEntsElement | undefined {
+    return this.#contextElementSignal[0]();
+  }
 
   constructor() {
     super();
 
-    this[$uuid] = generateUUID();
+    createEffect(() => {
+      console.log(`contextElement#${this.uuid}`, this.contextElement);
+    });
   }
 
   connectedCallback() {
-    console.log('Custom element added to page', this[$uuid]);
+    console.log('Custom element added to page', this.uuid);
 
     this.dispatchEvent(new CustomEvent(RequestContextEventName, {bubbles: true, detail: {requester: this}}));
 
     this.addEventListener(RequestContextEventName, this.#onRequestContext, {capture: false, passive: false});
-    this.addEventListener(ElementContextEventName, this.#onElementContext, {capture: false, passive: false});
   }
 
   disconnectedCallback() {
-    console.log('Custom element removed from page', this[$uuid]);
+    console.log('Custom element removed from page', this.uuid);
 
-    this.removeEventListener(ElementContextEventName, this.#onElementContext, {capture: false});
     this.removeEventListener(RequestContextEventName, this.#onRequestContext, {capture: false});
   }
 
   adoptedCallback() {
-    console.log('Custom element moved to new page', this[$uuid]);
+    console.log('Custom element moved to new page', this.uuid);
   }
 
   #onRequestContext = (event: RequestContextEvent) => {
     const requester = event.detail?.requester;
 
-    console.log('Custom element request context', {requester: requester?.[$uuid], self: this[$uuid]});
+    console.log('Custom element request context', {requester: requester?.uuid, self: this.uuid});
 
     if (requester != null && requester !== this) {
       console.log('Custom element stop propagation', requester);
@@ -48,10 +60,6 @@ export class ShadowEntsElement extends HTMLElement {
 
   #setContextElement(contextElement: ShadowEntsElement) {
     console.log('Custom element set context element', contextElement);
+    this.#contextElementSignal[1](contextElement ?? undefined);
   }
-
-  // TODO remove?
-  #onElementContext = (event: ElementContextEvent) => {
-    console.log('Custom element element context', event);
-  };
 }
--- packages/shadow-ents/src/elements/constants.ts
+++ packages/shadow-ents/src/elements/constants.ts
@@ -1,6 +1,4 @@
 export const $isElement = Symbol('isShadowEntsElement');
-
-export const $uuid = Symbol('seUuid');
 export const $type = Symbol('seType');
 
 export enum ElementType {
