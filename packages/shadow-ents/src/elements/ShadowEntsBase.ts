0df95e3837cfb615560d8b70d14a93b63d209751
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-17 08:59:37 +0200

    improve debug infos

--- packages/shadow-ents/src/elements/ShadowEntsBase.ts
+++ packages/shadow-ents/src/elements/ShadowEntsBase.ts
@@ -1,5 +1,6 @@
 import {createEffect, createSignal, type SignalReader, type SignalWriter} from '@spearwolf/signalize';
 import {generateUUID} from '../generateUUID.js';
+import {ComponentContext} from '../index.js';
 import {RequestContextEventName, ShadowElementType} from './constants.js';
 import type {RequestContextEvent} from './events.js';
 
@@ -14,14 +15,30 @@ export class ShadowEntsBase extends HTMLElement {
 
   readonly uuid = generateUUID();
 
-  #parentShadowElement$sig: [SignalReader<ShadowEntsBase | undefined>, SignalWriter<ShadowEntsBase | undefined>] = createSignal();
+  /** the shadow ents namespace */
+  get ns(): string | symbol {
+    return this.#namespace$sig[0]();
+  }
 
-  #setParentShadowElement(element: ShadowEntsBase) {
-    this.#parentShadowElement$sig[1](element ?? undefined);
+  set ns(value: string | symbol | null | undefined) {
+    this.#namespace$sig[1](
+      typeof value === 'string'
+        ? value.trim() || ComponentContext.GlobalNS
+        : typeof value === 'symbol'
+        ? value
+        : ComponentContext.GlobalNS,
+    );
   }
 
   get parentShadowElement(): ShadowEntsBase | undefined {
     return this.#parentShadowElement$sig[0]();
+  }
+
+  #namespace$sig: [SignalReader<string | symbol>, SignalWriter<string | symbol>] = createSignal();
+  #parentShadowElement$sig: [SignalReader<ShadowEntsBase | undefined>, SignalWriter<ShadowEntsBase | undefined>] = createSignal();
+
+  #setParentShadowElement(element: ShadowEntsBase) {
+    this.#parentShadowElement$sig[1](element ?? undefined);
   }
 
   constructor() {
@@ -29,25 +46,40 @@ export class ShadowEntsBase extends HTMLElement {
 
     // TODO remove me!
     createEffect(() => {
-      console.log(`parentShadowElement#${this.uuid}`, '->', this.parentShadowElement?.uuid, this.parentShadowElement);
+      const parent = this.parentShadowElement;
+      console.info(
+        `<shadow-ents-base ns=${String(this.ns)} uuid=${this.uuid}>`,
+        'parentShadowElement:',
+        parent ? {uuid: parent?.uuid, parent} : undefined,
+      );
     });
   }
 
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.debug('attributeChangedCallback', this.uuid, name, '=', oldValue, '->', newValue);
+
+    switch (name) {
+      case 'ns':
+        this.ns = newValue;
+        break;
+    }
+  }
+
   connectedCallback() {
-    console.log('connectedCallback', this.uuid);
+    console.debug('connectedCallback', this.uuid);
 
     this.dispatchRequestContextEvent();
     this.registerListener();
   }
 
   disconnectedCallback() {
-    console.log('disconnectedCallback', this.uuid);
+    console.debug('disconnectedCallback', this.uuid);
 
     this.unregisterListener();
   }
 
   adoptedCallback() {
-    console.log('TODO adoptedCallback', this.uuid);
+    console.warn('TODO adoptedCallback', this.uuid);
   }
 
   #onRequestContext = (event: RequestContextEvent) => {
