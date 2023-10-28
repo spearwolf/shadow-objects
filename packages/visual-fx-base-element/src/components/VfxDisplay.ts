c24fc8ea66b1cd71778368e59679c58bdd5760df
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-28 12:43:43 +0200

    vfx-display: add slots and console-logger

--- packages/visual-fx-base-element/src/components/VfxDisplay.ts
+++ packages/visual-fx-base-element/src/components/VfxDisplay.ts
@@ -3,36 +3,101 @@ import {eventize, type Eventize} from '@spearwolf/eventize';
 import {Display, type DisplayParameters} from '@spearwolf/twopoint5d';
 import {css, html, LitElement} from 'lit';
 import {twopoint5dDisplayContext} from '../context/twopoint5d-display-context.js';
+import {ConsoleLogger} from '../utils/ConsoleLogger.js';
 import {readBooleanAttribute} from '../utils/readBooleanAttribute.js';
 import {readStringAttribute} from '../utils/readStringAttribute.js';
 
 export interface VfxDisplay extends Eventize {}
 
 export class VfxDisplay extends LitElement {
+  static LoggerNS = 'vfx-display';
+
   static override styles = css`
     :host {
       display: block;
+    }
+    .layout {
+      position: relative;
+    }
+    .overlay-content {
+      display: block;
+      position: absolute;
+      overflow: hidden;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    .top-right {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+    .top-left {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .bottom-right {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+    .bottom-left {
+      position: absolute;
+      bottom: 0;
+      left: 0;
     }
   `;
 
   @provide({context: twopoint5dDisplayContext})
   accessor display: Display | undefined;
 
-  get container(): HTMLDivElement | undefined {
-    return this.renderRoot?.querySelector('div') ?? undefined;
+  get container(): HTMLElement | undefined {
+    return this.renderRoot?.querySelector('.canvas-container') ?? undefined;
   }
 
   get canvas(): HTMLCanvasElement | undefined {
     return this.renderRoot?.querySelector('canvas') ?? undefined;
   }
 
+  #debug = false;
+
+  get debug(): boolean {
+    return this.#debug;
+  }
+
+  set debug(value: boolean) {
+    this.#debug = value;
+    if (this.#debug && this.logger == null) {
+      this.logger = new ConsoleLogger(VfxDisplay.LoggerNS);
+    } else if (!this.#debug && this.logger != null) {
+      this.logger = undefined;
+    }
+  }
+
+  logger?: ConsoleLogger;
+
   constructor() {
     super();
     eventize(this);
+    this.debug = this.hasAttribute('debug');
+    // TODO react to attribute changes for debug
+    // TODO fullscreen - go fullscreen should include overlay-content
+    // TODO mobile fullscreen - go fullscreen on rotation to landscape (as attribute)
   }
 
   override render() {
-    return html`<div></div>`;
+    return html`<div class="layout">
+      <div class="canvas-container"></div>
+      <div class="overlay-content">
+        <slot></slot>
+        <div class="top-right"><slot name="top-right"></slot></div>
+        <div class="top-left"><slot name="top-left"></slot></div>
+        <div class="bottom-right"><slot name="bottom-right"></slot></div>
+        <div class="bottom-left"><slot name="bottom-left"></slot></div>
+      </div>
+    </div>`;
   }
 
   override firstUpdated(): void {
@@ -43,7 +108,7 @@ export class VfxDisplay extends LitElement {
     super.connectedCallback();
     if (this.display) {
       this.display.start();
-      console.debug('display started', this.display);
+      this.logger?.log('display started', this.display);
     }
   }
 
@@ -51,13 +116,13 @@ export class VfxDisplay extends LitElement {
     super.disconnectedCallback();
     if (this.display) {
       this.display.stop();
-      console.debug('display stopped', this.display);
+      this.logger?.log('display stopped', this.display);
     }
   }
 
   #createDisplay(): void {
     if (this.display) {
-      console.warn('display already created');
+      this.logger?.warn('display already created');
       return;
     }
 
@@ -77,13 +142,13 @@ export class VfxDisplay extends LitElement {
       resizeToAttributeEl: this,
     };
 
-    console.debug('webgl context attributes:', options);
+    this.logger?.log('webgl context attributes:', options);
 
     this.display = new Display(this.container!, options);
 
     this.display.on(this);
     this.display.start();
 
-    console.debug('display created', this.display);
+    this.logger?.log('display created', this.display);
   }
 }
