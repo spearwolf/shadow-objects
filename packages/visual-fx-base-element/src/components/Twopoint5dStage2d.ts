f1ed590b7702ada3149d5719b11ed074fd8d722c
Wolfger Schramm <wolfger@spearwolf.de>
2023-10-31 16:55:41 +0100

    WIP stage2d and signal-maps

--- packages/visual-fx-base-element/src/components/Twopoint5dStage2d.ts
+++ packages/visual-fx-base-element/src/components/Twopoint5dStage2d.ts
@@ -1,8 +1,10 @@
 import {consume} from '@lit/context';
-import {createSignal, value, type SignalFuncs, type SignalReader} from '@spearwolf/signalize';
+import {createEffect, createSignal, value, type SignalFuncs, type SignalReader} from '@spearwolf/signalize';
 import type {Display} from '@spearwolf/twopoint5d';
 import {css, html} from 'lit';
+import {property} from 'lit/decorators.js';
 import {twopoint5dDisplayContext} from '../context/twopoint5d-display-context.js';
+import {SignalMap} from '../utils/SignalMap.js';
 import {VisualFxBaseElement} from './VisualFxBaseElement.js';
 
 export class Twopoint5dStage2d extends VisualFxBaseElement {
@@ -29,17 +31,100 @@ export class Twopoint5dStage2d extends VisualFxBaseElement {
     this.#display[1](value);
   }
 
+  @property({type: String, reflect: true})
+  accessor fit: 'contain' | 'cover' | 'fill' | undefined;
+
+  @property({type: Number, reflect: true})
+  accessor width: number | undefined;
+
+  @property({type: Number, reflect: true})
+  accessor height: number | undefined;
+
+  @property({type: Number, reflect: true, attribute: 'pixel-zoom'})
+  accessor pixelZoom: number | undefined;
+
+  @property({type: Number, reflect: true, attribute: 'min-pixel-zoom'})
+  accessor minPixelZoom: number | undefined;
+
+  @property({type: Number, reflect: true, attribute: 'max-pixel-zoom'})
+  accessor maxPixelZoom: number | undefined;
+
+  @property({type: Number, reflect: true})
+  accessor near: number | undefined;
+
+  @property({type: Number, reflect: true})
+  accessor far: number | undefined;
+
+  @property({type: Number, reflect: true, attribute: 'distance-to-projection-plane'})
+  accessor distanceToProjectionPlane: number | undefined;
+
+  @property({type: String, reflect: true, attribute: 'projection-plane'})
+  accessor projectionPlane: 'xy' | 'xz' | undefined;
+
+  @property({type: String, reflect: true, attribute: 'projection-origin'})
+  accessor projectionOrigin: 'bottom-left' | 'top-left' | undefined;
+
+  @property({type: String, reflect: true, attribute: 'projection-type'})
+  accessor projectionType: 'parallax' | 'ortho' | 'orthographic' | undefined;
+
+  readonly #projSignals: SignalMap;
+  readonly #viewSpecsSignals: SignalMap;
+
   constructor() {
     super();
 
     this.loggerNS = 'twopoint5d-stage2d';
 
+    this.fit = 'contain';
+
+    this.projectionPlane = 'xy';
+    this.projectionOrigin = 'bottom-left';
+
     this.display$((display) => this.logger?.log('display updated:', display));
+
+    this.#projSignals = SignalMap.fromProps(this, ['projectionPlane', 'projectionOrigin', 'projectionType']);
+
+    this.#viewSpecsSignals = SignalMap.fromProps(this, [
+      'fit',
+      'width',
+      'height',
+      'pixelZoom',
+      'minPixelZoom',
+      'maxPixelZoom',
+      'near',
+      'far',
+      'distanceToProjectionPlane',
+    ]);
+
+    createEffect(() => {
+      this.onProjectionUpdate();
+    }, this.#projSignals.getSignals());
+
+    createEffect(() => {
+      this.onViewSpecsUpdate();
+    }, this.#viewSpecsSignals.getSignals());
+  }
+
+  override willUpdate(changedProperties: Map<PropertyKey, unknown>): void {
+    this.logger?.log('willUpdate', changedProperties);
+
+    const propKeys = Array.from(changedProperties.keys()) as (keyof this)[];
+
+    this.#projSignals.updateFromProps(this, propKeys);
+    this.#viewSpecsSignals.updateFromProps(this, propKeys);
   }
 
   override render() {
     this.display = this.displayCtx;
 
     return html`<slot></slot>`;
+  }
+
+  private onProjectionUpdate(): void {
+    this.logger?.log('projection update', this.#projSignals.getValueObject());
+  }
+
+  private onViewSpecsUpdate(): void {
+    this.logger?.log('view-specs update', this.#viewSpecsSignals.getValueObject());
   }
 }
