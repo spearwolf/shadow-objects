f43f39eddec2383d28575942e19f6edae407efcd
Wolfger Schramm <wolfger@spearwolf.de>
2023-11-01 13:32:00 +0100

    attach lit context-root to doc body

--- packages/visual-fx-base-element/src/twopoint5d-stage2d.ts
+++ packages/visual-fx-base-element/src/twopoint5d-stage2d.ts
@@ -1,3 +1,4 @@
 import {Twopoint5dStage2d} from './components/Twopoint5dStage2d.js';
+import './components/attachContextRoot.js';
 
 customElements.define('two.5d-stage2d', Twopoint5dStage2d);
--- packages/visual-fx-base-element/src/vfx-display.ts
+++ packages/visual-fx-base-element/src/vfx-display.ts
@@ -1,3 +1,4 @@
 import {VfxDisplay} from './components/VfxDisplay.js';
+import './components/attachContextRoot.js';
 
 customElements.define('vfx-display', VfxDisplay);
