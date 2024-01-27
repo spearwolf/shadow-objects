47dac5894a98042d8c30070052097bfd5639e37e
Wolfger Schramm <wolfger@spearwolf.de>
2024-01-27 13:27:48 +0100

    Remove sum.js and sum.test.js

--- packages/shadow-ents-testing/test/sum.test.js
+++
@@ -1,7 +0,0 @@
-import {expect} from '@esm-bundle/chai';
-import {sum} from '../src/sum.js';
-
-it('sums up 2 numbers', () => {
-  expect(sum(1, 1)).to.equal(2);
-  expect(sum(3, 12)).to.equal(15);
-});
