a0735defd4835109c0e6ccc011492a0bc19159d7
Wolfger Schramm <wolfger@spearwolf.de>
2024-05-30 17:28:42 +0200

    just use structuredClone with transfer option

--- packages/shadow-ents/src/view/cloneChangeTrail.ts
+++ packages/shadow-ents/src/view/cloneChangeTrail.ts
@@ -1,35 +1,10 @@
-import type {ChangeTrailType, IComponentChangeType} from '../types.js';
-
-const cloneData = (data: any, transferables: Set<object>): any => {
-  if (Array.isArray(data)) {
-    return data.map((item) => cloneData(item, transferables));
-  }
-
-  if (typeof data === 'object' && data !== null) {
-    if (transferables.has(data)) {
-      return data;
-    }
-    return Object.fromEntries(Object.entries(data).map(([key, value]) => [key, cloneData(value, transferables)]));
-  }
-
-  if (typeof data === 'function') {
-    return undefined;
-  }
-
-  return data;
-};
-
-function partialClone({transferables, ...trail}: IComponentChangeType): IComponentChangeType {
-  const allTransferables = new Set(transferables);
-  const dolly = cloneData(trail, allTransferables);
-  allTransferables.clear();
-  return dolly;
-}
+import type {ChangeTrailType} from '../types.js';
 
 export function cloneChangeTrail(trails: ChangeTrailType): ChangeTrailType {
   return trails.map((trail) => {
     if (trail.transferables && trail.transferables.length > 0) {
-      return partialClone(trail);
+      const {transferables, ...data} = trail;
+      return structuredClone(data, {transfer: transferables as any});
     } else {
       return structuredClone(trail);
     }
