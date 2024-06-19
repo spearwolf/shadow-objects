d72c2a6dade7fec12402cd256a05a79267bb3b02
spearwolf <wolfger@spearwolf.de>
2024-06-19 09:19:18 +0200

    RemoteWorkerEnv WIP

--- packages/shadow-ents/src/view/RemoteWorkerEnv.ts
+++ packages/shadow-ents/src/view/RemoteWorkerEnv.ts
@@ -1,3 +1,4 @@
+import {eventize, type EventizeApi} from '@spearwolf/eventize';
 import {
   Configure,
   Destroy,
@@ -9,27 +10,78 @@ import {
   WorkerLoadTimeout,
 } from '../constants.js';
 import {waitForMessageOfType} from '../elements/waitForMessageOfType.js';
-import type {ChangeTrailType} from '../types.js';
+import type {ChangeTrailType, SyncEvent, TransferablesType} from '../types.js';
 import type {IShadowObjectEnvProxy} from './IShadowObjectEnvProxy.js';
 
+const prepareChangeTrail = (data: SyncEvent): [SyncEvent, TransferablesType | undefined] => {
+  let transferables: TransferablesType | undefined;
+
+  if (Array.isArray(data.changeTrail)) {
+    for (const changeItem of data.changeTrail) {
+      if (changeItem.transferables) {
+        if (!transferables) {
+          transferables = changeItem.transferables;
+        } else {
+          transferables = [...transferables, ...changeItem.transferables];
+        }
+        delete changeItem.transferables;
+      }
+    }
+  }
+
+  return [data, transferables];
+};
+
+export interface RemoteWorkerEnv extends EventizeApi {}
+
 export class RemoteWorkerEnv implements IShadowObjectEnvProxy {
+  static WorkerLoaded = 'workerLoaded';
+
   static createWorker() {
     return new Worker(new URL('../shadow-ents.worker.js', import.meta.url), {type: 'module'});
   }
 
   #worker?: Worker;
+  #isDestroyed = false;
 
-  async start() {
+  get isDestroyed(): boolean {
+    return this.#isDestroyed;
+  }
+
+  get workerLoaded(): Promise<RemoteWorkerEnv> {
+    return this.onceAsync<RemoteWorkerEnv>(RemoteWorkerEnv.WorkerLoaded);
+  }
+
+  constructor() {
+    eventize(this);
+    this.retain(RemoteWorkerEnv.WorkerLoaded);
+  }
+
+  async start(): Promise<void> {
     if (this.#worker) {
       console.warn('RemoteWorkerEnv: already started');
-      return;
+      return this.workerLoaded.then(() => {
+        if (this.isDestroyed) {
+          throw 'RemoteWorkerEnv: worker was destoyed';
+        }
+        return undefined;
+      });
     }
 
     const worker = (this.#worker = RemoteWorkerEnv.createWorker());
 
     try {
       await waitForMessageOfType(worker, Loaded, WorkerLoadTimeout);
+
+      if (this.isDestroyed) {
+        throw 'RemoteWorkerEnv: worker was destoyed';
+      }
+
       worker.addEventListener('message', this.onMessageFromWorker.bind(this));
+
+      queueMicrotask(() => {
+        this.emit(RemoteWorkerEnv.WorkerLoaded, this);
+      });
     } catch (error) {
       console.error('RemoteWorkerEnv: failed to start', error);
       this.#worker = undefined;
@@ -42,8 +94,8 @@ export class RemoteWorkerEnv implements IShadowObjectEnvProxy {
     console.log('RemoteWorkerEnv: message from worker', event);
   }
 
-  applyChangeTrail(_data: ChangeTrailType): Promise<void> {
-    // TODO implement applyChangeTrail
+  applyChangeTrail(data: ChangeTrailType): Promise<void> {
+    // const [changeTrailData, transfer] = prepareChangeTrail(data);
     return Promise.resolve();
   }
 
@@ -57,6 +109,8 @@ export class RemoteWorkerEnv implements IShadowObjectEnvProxy {
 
     const worker = this.#worker;
     this.#worker = undefined;
+
+    this.#isDestroyed = true;
 
     worker.postMessage({type: Destroy});
 
